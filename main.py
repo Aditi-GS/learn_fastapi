448185b06bba27371d25482a89c3010fc4126431
Aditi G S (CW) <Aditig.S@blueyonder.com>
2026-03-11 11:44:45 +0530

    schema

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 def root():
     return {"message": "I have no idea what to type :("}
 
 @app.post("/posts")
-def create_post(payload: dict = Body(...)):
-    return {"new_post": f"Title: {payload['title']} AND Content: {payload['content']}"}
+def create_post(post: Post):
+    return  {"data": post.model_dump()}
